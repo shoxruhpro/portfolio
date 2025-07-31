917866803a7cad3f97b8ad56b2f4438d3f9dbde8
Shokhrukh Rustamaliev <edulink.uz@outlook.com>
2025-07-31 09:29:45 +0500

    Yopish tugmasi chiqarildi.

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const menu = document.querySelector("#menu")
 
 burgerButton.addEventListener("click", () => {
   menu.style.display = "block"
+  burgerButton.classList.remove("burger")
+  burgerButton.classList.add("close")
 })
