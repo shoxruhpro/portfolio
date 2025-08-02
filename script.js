45dd113ee8f46674a016862dd14b53ce38e2dada
Shokhrukh Rustamaliev <edulink.uz@outlook.com>
2025-08-02 08:59:57 +0500

    Menu yopiladigan qilindi.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const burgerButton = document.querySelector("#burger")
 const menu = document.querySelector("#menu")
 
+let isOpen = false
+
 burgerButton.addEventListener("click", () => {
-  menu.style.display = "block"
-  burgerButton.classList.remove("burger")
-  burgerButton.classList.add("close")
+  if (isOpen) {
+    menu.style.display = "none"
+    burgerButton.classList.remove("close")
+    burgerButton.classList.add("burger")
+  } else {
+    menu.style.display = "block"
+    burgerButton.classList.remove("burger")
+    burgerButton.classList.add("close")
+  }
+  isOpen = !isOpen
 })
